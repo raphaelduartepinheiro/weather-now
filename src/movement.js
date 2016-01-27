33b932d9203ae4c0e4e05236c30e51c3965c23ae
Raphael D. Pinheiro <raphaklaus@gmail.com>
2016-01-27 20:32:52 -0200

    Integrated with weather API

--- src/movement.js
+++ src/movement.js
@@ -7,11 +7,25 @@
 //   }
 // }).run();
 
-$.ajax({
-  type: 'GET',
-  url: 'http://api.openweathermap.org/data/2.5/weather?q=Curitiba&appid=44db6a862fba0b067b1930da0d769e98',
-  dataType: 'json',
-  success: function(data){
-    $('.forecast').text(data.weather[0].main);
+function draw(weather){
+
+}
+
+function send(event){
+  if (event.keyCode === 13){
+    var city = encodeURI($('.city').val());
+
+    $.ajax({
+      type: 'GET',
+      url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=44db6a862fba0b067b1930da0d769e98',
+      dataType: 'json',
+      success: function(data){
+        if (data.cod === 200)
+          $('.forecast').text(data.weather[0].main);
+        else
+          $('.forecast').text('City not found :(');
+      }
+    });  
   }
-});
+}
+
