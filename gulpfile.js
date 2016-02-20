61173bbedcfa420dcb42fe9f3f36ae45ac196696
Raphael D. Pinheiro <raphaklaus@gmail.com>
2016-02-20 02:20:31 -0200

    Implemented gulp tasks :heart:

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+var browserSync = require('browser-sync');
+var autoprefixer = require('autoprefixer-stylus');
+var minifyCss = require('gulp-minify-css');
+var gulpStylint = require('gulp-stylint');
+var reload = browserSync.reload;
+var del = require('del');
+
+gulp.task('clean', function(){
+  return del(['build']);
+});
+
+gulp.task('browser-sync', function(){
+  browserSync({
+    server:{
+      baseDir: ""
+    }
+  })
+});
+
+gulp.task('refresh-browser', function(){
+  reload({stream:true});
+})
+
+gulp.task('compile-css', ['clean'], function(){
+  return gulp.src('./css/styles.styl')
+    .pipe(gulpStylint())
+    .pipe(gulpStylint.reporter())
+    .pipe(stylus({
+      use: [autoprefixer()]
+    }))
+    .pipe(gulp.dest('./css/build'))
+    .pipe(reload({stream:true}));
+});
+
+gulp.task('dist', function(){
+  return gulp.src('./css/build/styles.css')
+    .pipe(minifyCss())
+    .pipe(gulp.dest('./css/build/'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./css/styles.styl', ['compile-css', 'dist']);
+  gulp.watch('./index.html', ['browser-sync']);
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
